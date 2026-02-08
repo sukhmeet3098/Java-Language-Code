7280ed9b9cecf45168bc21bf0686dd3cb90e950a
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-07 23:56:22 -0500

    Output In Java

---
+++ OutputInJava.java
@@ -0,0 +1,13 @@
+public class OutputInJava {
+  public static void main(String[] args) {
+    // Output with print function
+    System.out.print("Hello World with Java");
+    // Output with println
+    System.out.println("Hello World with Java");
+    System.out.print("Hello World with Java");
+    // Output with \n 
+    System.out.print("Hello World with Java\n");
+    System.out.print("Hello World with Java\n");
+    System.out.print("Hello World with Java");
+  }
+}
