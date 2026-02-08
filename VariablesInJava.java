16f8b214c3ac7d591ec4b6a3296518170b5332c7
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-08 00:30:32 -0500

    Variables and DataTypes In Java

---
+++ VariablesInJava.java
@@ -0,0 +1,14 @@
+public class VariablesInJava {
+  public static void main(String[] args) {
+    // Constant is Fixed Values that donot change
+
+    // Variables are store container where we can store the changing values
+    // Here int is a integer datatype. In Java there are 8 Primitive Datatypes (byte, short, int, float, long, double, char, boolean) and their sizes are in bytes (1 byte = 8 bits) (1, 1, 4, 4, 8, 8, 2, 1).
+    
+    int a = 10;
+    int b = 25;
+    int sum = a + b;
+    System.out.println(sum);
+    System.out.println(a * b);
+  }
+}
