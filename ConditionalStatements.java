24248c1f6e0fba18bdb6122c50b503ff2b80d15e
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-08 22:34:01 -0500

    Elseif ConditionDone

--- ConditionalStatements.java
+++ ConditionalStatements.java
@@ -3,15 +3,42 @@ import java.util.Scanner;
 public class ConditionalStatements {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int age = sc.nextInt();
 
-    if(age > 18) {
+    // If-else Statement
+    
+   //int age = sc.nextInt();
+
+    /*if(age > 18) {
       System.out.println("Adult");
     }
 
     else {
       System.out.println("Not Adult");
+    }*/
+
+    /*int num = sc.nextInt();
+
+    if ((num % 2) == 0) {
+      System.out.println("Number is Even");
     }
-    
+
+    else {
+      System.out.println("Number is odd");
+    }*/
+
+    // If-Elseif-Else Statements
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    if(a==b) {
+      System.out.println("Equal");
+    }
+    else if (a > b){
+      System.out.println("a is greater");
+    }
+    else{
+      System.out.println("a is lesser");
+    }
   }
 }
