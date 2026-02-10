b6a9ba6fb6f87f57da9c3014ab21a17d433a8b2a
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-10 17:38:15 -0500

    Loop Practice Questions Solved

--- Loops.java
+++ Loops.java
@@ -1,4 +1,4 @@
-import javax.swing.plaf.synth.SynthPasswordFieldUI;
+import java.util.Scanner;
 
 public class Loops {
   public static void main(String[] args) {
@@ -33,16 +33,33 @@ public class Loops {
     // }while(i <= 10);
 
     // Check the difference between while and do while loop
-    int i = 12;
+    // int i = 12;
 
-    while (i < 11) {
-      System.out.println("Sukhmeet Singh");
-      i = i + 1;
+    // while (i < 11) {
+    //   System.out.println("Sukhmeet Singh");
+    //   i = i + 1;
+    // }
+
+    // do {
+    //   System.out.println("Sukhmeet Singh");
+    //   i = i + 1;
+    // } while(i < 11);
+
+    // Print the sum of first n natural numbers.
+    Scanner sc = new Scanner(System.in);
+    // int n = sc.nextInt();
+    // int sum = 0;
+
+    // for(int i = 1; i <= n; i = i + 1){
+    //   sum = sum + i;
+    // }
+    // System.out.println(sum);
+
+    // Print the table of a number input by the user.
+    int n = sc.nextInt();
+
+    for (int i = 1; i <= 10; i = i + 1){
+      System.out.println("2 * " + i + " : " + (n * i));
     }
-
-    do {
-      System.out.println("Sukhmeet Singh");
-      i = i + 1;
-    } while(i < 11);
   }
 }
