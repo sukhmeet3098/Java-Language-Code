f9866473bf1a05ad3160b0d7d633624ef2f5d32d
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-10 17:05:21 -0500

    Practice Question Solved

--- Loops.java
+++ Loops.java
@@ -1,7 +1,16 @@
 public class Loops {
   public static void main(String[] args) {
+    // For Loop Intialization
     for(int counter = 0; counter < 3; counter = counter + 1) {
       System.out.println("Hello World");
     }
+    
+    // Pratice Question
+    // counter ++ = counter = counter + 1
+    for(int counter = 0; counter <= 10; counter ++){
+      //System.out.println(counter);
+      System.out.print(counter + " ");
+    }
+
   }
 }
