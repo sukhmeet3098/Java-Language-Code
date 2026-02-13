e74eb71d1abfcb56083138075447ee93de43ab26
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-12 22:18:47 -0500

    Number Pyramid  Pattern Solved

--- AdvancedPatterns.java
+++ AdvancedPatterns.java
@@ -1,57 +1,73 @@
 public class AdvancedPatterns {
   public static void main(String[] args) {
     // Butterfly Pattern
-    int n = 4;
+    // int n = 4;
 
-    // Upper Part
-    for(int i = 1; i <= n; i++){
-      // First Star Part
-      for(int j = 1; j <= i; j++){
-        System.out.print("*");
-      }
-      // Spaces Part
-      int spaces = 2*(n-i);
-      for(int j = 1; j <= spaces; j++){
-        System.out.print(" ");
-      }
-      // Second Star Part
-      for(int j = 1; j <= i; j++){
-        System.out.print("*");
-      }
-      System.out.println();
-    }
+    // // Upper Part
+    // for(int i = 1; i <= n; i++){
+    //   // First Star Part
+    //   for(int j = 1; j <= i; j++){
+    //     System.out.print("*");
+    //   }
+    //   // Spaces Part
+    //   int spaces = 2*(n-i);
+    //   for(int j = 1; j <= spaces; j++){
+    //     System.out.print(" ");
+    //   }
+    //   // Second Star Part
+    //   for(int j = 1; j <= i; j++){
+    //     System.out.print("*");
+    //   }
+    //   System.out.println();
+    // }
     
-    // Lower Part
-    for(int i = n; i >= 1; i--){
-      // First Star Part
-      for(int j = 1; j <= i; j++){
-        System.out.print("*");
-      }
-      // Spaces Part
-      int spaces = 2*(n-i);
-      for(int j = 1; j <= spaces; j++){
-        System.out.print(" ");
-      }
-      // Second Star Part
-      for(int j = 1; j <= i; j++){
-        System.out.print("*");
-      }
-      System.out.println();
-    }
+    // // Lower Part
+    // for(int i = n; i >= 1; i--){
+    //   // First Star Part
+    //   for(int j = 1; j <= i; j++){
+    //     System.out.print("*");
+    //   }
+    //   // Spaces Part
+    //   int spaces = 2*(n-i);
+    //   for(int j = 1; j <= spaces; j++){
+    //     System.out.print(" ");
+    //   }
+    //   // Second Star Part
+    //   for(int j = 1; j <= i; j++){
+    //     System.out.print("*");
+    //   }
+    //   System.out.println();
+    // }
 
     // Solid Rhombus Pattern
-    int num = 5;
+    // int num = 5;
 
-    for(int i = 1; i<=num; i++){
-      //Spaces Part
-      for(int j = 1; j <= (num-i); j++){
+    // for(int i = 1; i<=num; i++){
+    //   //Spaces Part
+    //   for(int j = 1; j <= (num-i); j++){
+    //     System.out.print(" ");
+    //   }
+    //   //Stars Part
+    //   for(int j = 1; j <= num; j++){
+    //     System.out.print("*");
+    //   }
+    //   System.out.println();
+    // }
+    
+    // Number Pyramid Pattern
+    int n = 5;
+
+    for(int i = 0; i <= n; i++){
+      // Spaces Part
+      for(int j = 1; j <= (n-i); j++){
         System.out.print(" ");
       }
-      //Stars Part
-      for(int j = 1; j <= num; j++){
-        System.out.print("*");
+      // Number Part
+      for(int j = 1; j <= i; j++){
+        System.out.print(i + " ");
       }
       System.out.println();
     }
+
   }
 }
