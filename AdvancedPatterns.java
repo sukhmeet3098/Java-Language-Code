a75d63107ae0dcda8638328ddea9a590fc493f6f
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-12 21:51:27 -0500

    Butterfly Pattern Solved

---
+++ AdvancedPatterns.java
@@ -0,0 +1,42 @@
+public class AdvancedPatterns {
+  public static void main(String[] args) {
+    // Butterfly Pattern
+    int n = 4;
+
+    // Upper Part
+    for(int i = 1; i <= n; i++){
+      // First Star Part
+      for(int j = 1; j <= i; j++){
+        System.out.print("*");
+      }
+      // Spaces Part
+      int spaces = 2*(n-i);
+      for(int j = 1; j <= spaces; j++){
+        System.out.print(" ");
+      }
+      // Second Star Part
+      for(int j = 1; j <= i; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    
+    // Lower Part
+    for(int i = n; i >= 1; i--){
+      // First Star Part
+      for(int j = 1; j <= i; j++){
+        System.out.print("*");
+      }
+      // Spaces Part
+      int spaces = 2*(n-i);
+      for(int j = 1; j <= spaces; j++){
+        System.out.print(" ");
+      }
+      // Second Star Part
+      for(int j = 1; j <= i; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
