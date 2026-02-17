853b767158468f61875169385aa1dbc2c60ae3b5
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-16 23:30:47 -0500

    Second Problem Solved

--- JavaFirstExercise.java
+++ JavaFirstExercise.java
@@ -8,15 +8,36 @@ public class JavaFirstExercise {
     return average;
   }
 
+  public static void sumOfOdd(int a) {
+    int sum = 0;
+    if (a > 0) {
+      for (int i = 1; i <= a; i++) {
+        if ((i % 2) != 0) {
+          sum = sum + i;
+        }
+      }
+    } else {
+      System.out.println("Number must be greater than zero.");
+      return;
+    }
+    System.out.println(sum);
+    return;
+  }
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
     // Enter 3 numbers from the user & make a function to print their average.
-    float num1 = sc.nextInt();
-    float num2 = sc.nextInt();
-    float num3 = sc.nextInt();
+    // float num1 = sc.nextInt();
+    // float num2 = sc.nextInt();
+    // float num3 = sc.nextInt();
 
-    float average = findAverage(num1, num2, num3);
-    System.out.println("Average is : " + average);
+    // float average = findAverage(num1, num2, num3);
+    // System.out.println("Average is : " + average);
+
+    // Write a function to print the sum of all odd numbers from 1 to n.
+    int num = sc.nextInt();
+    sumOfOdd(num);
+
   }
 }
