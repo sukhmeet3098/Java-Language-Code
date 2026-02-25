62cdfaca2eee3ea35ef6e12fdec03103563a6f09
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-24 22:58:27 -0500

    Transpose 2D Array Print Problem Solved

--- Arrays2DHomework.java
+++ Arrays2DHomework.java
@@ -14,41 +14,50 @@ public class Arrays2DHomework {
       }
     }
 
-    int count = 1;
-    int rowStart = 0;
-    int colStart = 0;
-    int rowEnd = rows - 1;
-    int colEnd = cols - 1;
+    // int count = 1;
+    // int rowStart = 0;
+    // int colStart = 0;
+    // int rowEnd = rows - 1;
+    // int colEnd = cols - 1;
 
-    for (int i = 0; i < (2 * rows); i++) {
-      if ((rowStart <= rowEnd) && (colStart <= colEnd)) {
-        if (count == 1) {
-          for (int j = colStart; j <= colEnd; j++) {
-            System.out.print(martrix[rowStart][j] + " ");
-          }
-          count = 2;
-          rowStart++;
-        } else if (count == 2) {
-          for (int j = rowStart; j <= rowEnd; j++) {
-            System.out.print(martrix[j][colEnd] + " ");
-          }
-          count = 3;
-          colEnd--;
-        } else if (count == 3) {
-          for (int j = colEnd; j >= colStart; j--) {
-            System.out.print(martrix[rowEnd][j] + " ");
-          }
-          count = 4;
-          rowEnd--;
-        } else {
-          for (int j = rowEnd; j >= rowStart; j--) {
-            System.out.print(martrix[j][colStart] + " ");
-          }
-          count = 1;
-          colStart++;
-        }
+    // for (int i = 0; i < (2 * rows); i++) {
+    // if ((rowStart <= rowEnd) && (colStart <= colEnd)) {
+    // if (count == 1) {
+    // for (int j = colStart; j <= colEnd; j++) {
+    // System.out.print(martrix[rowStart][j] + " ");
+    // }
+    // count = 2;
+    // rowStart++;
+    // } else if (count == 2) {
+    // for (int j = rowStart; j <= rowEnd; j++) {
+    // System.out.print(martrix[j][colEnd] + " ");
+    // }
+    // count = 3;
+    // colEnd--;
+    // } else if (count == 3) {
+    // for (int j = colEnd; j >= colStart; j--) {
+    // System.out.print(martrix[rowEnd][j] + " ");
+    // }
+    // count = 4;
+    // rowEnd--;
+    // } else {
+    // for (int j = rowEnd; j >= rowStart; j--) {
+    // System.out.print(martrix[j][colStart] + " ");
+    // }
+    // count = 1;
+    // colStart++;
+    // }
 
+    // }
+    // }
+
+    // Print The Transpose of the Given Matrix
+
+    for (int j = 0; j < cols; j++) {
+      for (int i = 0; i < rows; i++) {
+        System.out.print(martrix[i][j] + " ");
       }
+      System.out.println();
     }
   }
 }
